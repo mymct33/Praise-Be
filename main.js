531cc3e6afa2f7010e09ab15c28860e002d4b53d
Connor S <mymct33@gmail.com>
2020-04-22 18:57:40 +1000

    Added another buy. will tweak later

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var credits = 0;
 var cursors = 0;
+var objects = 0;
 var prestige = 0;
 
 function creditClick(number){
@@ -19,10 +20,23 @@ function buyCursor(){
     };
 };
 
+function buyObject(){
+    var objectCost = Math.floor(10*Math.pow(6.9,objects))
+    if(credits >= objectCost){
+        objects = objects + 1;
+        credits = credits - objectCost;
+        document.getElementById('objects').innerHTML = objects;
+        document.getElementById('credits').innerHTML = prettify(credits);
+        var objectCost = Math.floor(10*Math.pow(6.9,objects))
+        document.getElementById('objectCost').innerHTML = objectCost;
+    }
+}
+
 function save(){
     var save = {
         credits: credits,
         cursors: cursors,
+        objects: objects,
         prestige: prestige
     };
     localStorage.setItem("save",JSON.stringify(save));
@@ -35,6 +49,11 @@ function load(){
         cursors = savegame.cursors;
         cursorCost = Math.floor(10*Math.pow(1.1,cursors));
         document.getElementById('cursorCost').innerHTML = cursorCost;
+    };
+    if(typeof savegame.objects != "undefined"){
+        objects = savegame.objects;
+        objectCost = Math.floor(10*Math.pow(6.9,objects));
+        document.getElementById('objectCost').innerHTML = objectCost;
     };
     if(typeof savegame.prestige !== "undefined") prestige = savegame.prestige;
 };
@@ -50,7 +69,9 @@ function prettify(input){
 
 window.setInterval(function(){
     creditClick(cursors);
+    creditClick(6.9*objects);
 
     document.getElementById('credits').innerHTML = prettify(credits);
     document.getElementById('cursors').innerHTML = cursors;
+    document.getElementById('objects').innerHTML = objects;
 }, 1000);
